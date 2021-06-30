684ea57f3a133bae3785227ed5a9d694536a75a7
Anand Shukla <anands2807@gmail.com>
2021-06-30 08:34:08 +0530

    issue resolved, further work in progress.

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -10,4 +10,4 @@ function getAllPlanets(req, res){
 
 module.exports = {
   getAllPlanets,
-}
+};
--- server/src/routes/planets/planets.router.js
+++ server/src/routes/planets/planets.router.js
@@ -1,13 +1,19 @@
 const express = require('express');
 
+
+/*
+
 const planetsController = require('./planets.controller');
-/*we can write the above constant using destructuring too: 
+
+
+we can write the above constant using destructuring too: 
+*/
 
 const {
   getAllPlanets,
 } = require('./planets.controller');
 
-This is a better way because we can use multiple functions from the same module.*/
+/*This is a better way because we can use multiple functions from the same module.*/
 
 const planetsRouter = express.Router();
 planetsRouter.get('/planets', getAllPlanets);
