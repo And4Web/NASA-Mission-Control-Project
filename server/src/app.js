0c3aebceade8b47616e61b0a79070a23d66bccf7
Anand Shukla <anands2807@gmail.com>
2021-07-15 09:55:50 +0530

    creating launches endpoint for schedulling upcoming launches.

--- server/src/app.js
+++ server/src/app.js
@@ -1,14 +1,18 @@
 const path = require('path');
 const express = require('express');
+const morgan = require('morgan');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
-
+ 
 const app = express();
 
 //chain of middlewares:
 app.use(cors({
   origin: 'http://localhost:3000',
 }));//CORS middleware to make cross-origin requests.
+
+app.use(morgan('combined'));
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
