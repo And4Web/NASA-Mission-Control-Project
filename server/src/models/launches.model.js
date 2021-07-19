cd2dd68356118b623ea3fd26492368689bfebae3
Anand Shukla <anands2807@gmail.com>
2021-07-19 15:15:46 +0530

    updated launches.model.js

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,14 +1,16 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: 'Kepler Exploration 2021',
   rocket: 'GSLV-2021',
   launchDate: new Date('July 28, 2021'),
   destination: 'Kepler-442 b',
-  customer: ['ISRO', 'IIT-D', 'IIT-B', 'TIFR', 'IISC-Banglore'],
-  upcoming: true,
-  success: true,
+  //customer: ['ISRO', 'IIT-D', 'IIT-B', 'TIFR', 'IISC-Banglore'],
+  //upcoming: true,
+  //success: true,
 
 };
 
@@ -20,6 +22,18 @@ function getAllLaunches(){
   return Array.from(launches.values());
 }
 
+function addNewLaunch(launch){
+  latestFlightNumber++;
+  launches.set(latestFlightNumber, Object.assign(launch, {
+    flightNumber: latestFlightNumber,
+    customer: ['ISRO', 'IIT-D', 'IIT-B', 'TIFR', 'IISC-Banglore'],
+    upcoming: true,
+    success: true,
+  })
+  );
+}
+
 module.exports = {
   getAllLaunches,
+  addNewLaunch,
 };
