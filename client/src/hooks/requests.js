986b8d0e31bb9e6c8599050609f414cf43234821
Anand Shukla <anands2807@gmail.com>
2021-07-21 17:40:21 +0530

    launches endpoint added to front end.

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,24 +1,39 @@
 const API_URL = 'http://localhost:8000';
 
-// Load planets and return as JSON.
+// Load planets and return as JSON:
 async function httpGetPlanets() {
-  const response = await fetch(`${API_URL}/planets`); // TODO: Once API is ready.
+  const response = await fetch(`${API_URL}/planets`); // TODO: Once API is ready. It's Done.
   return await response.json();
   
 }
 
 // Load launches, sort by flight number, and return as JSON:
 async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);// TODO: Once API is ready:
+  const response = await fetch(`${API_URL}/launches`);// TODO: Once API is ready: It's Done.
   const fetchedLaunches = await response.json();
   return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber;
   });//sort() function is used to get ascending order of launched flight numbers.  
 }
 
-async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+// 'Submit' given launch data to launch system:
+async function httpSubmitLaunch(launch){
+  try{
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(launch),
+    })
+  // TODO: Once API is ready. It's Done.
+  
+  }catch(err){
+    return {
+      ok: false,
+    };
+  };
+      
 }
 
 async function httpAbortLaunch(id) {
