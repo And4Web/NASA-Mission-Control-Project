625c152f36c1fe9768bfcdc8a1893d86d4453e5f
Anand Shukla <anands2807@gmail.com>
2021-07-31 18:21:07 +0530

    changes

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -3,13 +3,26 @@ const app = require('../../app');
 
 describe('Test GET /launches', () => {
   test('It should respond with 200 success', async () => {
-    const response = await request(app).get('./launches').expect(200);
+    const response = await request(app).get('./launches').expect('Content-Type', /json/).expect(200);
     //expect(response.statusCode).toBe(200);
   });
 });
 
 describe('Test POST /launches', () => {
-  test('It should respond with response 200 success', () => {
+  test('It should respond with response 201 created', async () => {
+    const response = await request(app).post('/launches').send({
+      mission: 'Mission-101',
+      rocket: 'GSLV-16D',
+      target: 'Kepler-186 f',
+      launchDate: 'January 4, 2028',
+    }).expect('Content-Type', /json/).expect(201);
+
+    expect(response.body).toMatchObject({
+      mission: 'Mission-101',
+      rocket: 'GSLV-16D',
+      target: 'Kepler-186 f',
+      launchDate: 'January 4, 2028',
+    });
 
   });
 
