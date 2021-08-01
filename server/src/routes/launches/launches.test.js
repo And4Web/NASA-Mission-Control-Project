4772279b9e081690df1a6c280615e3750281902d
Anand Shukla <anands2807@gmail.com>
2021-08-01 13:37:19 +0530

    date setting in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('Test GET /launches', () => {
+  
   test('It should respond with 200 success', async () => {
     const response = await request(app).get('./launches').expect('Content-Type', /json/).expect(200);
     //expect(response.statusCode).toBe(200);
@@ -9,20 +10,29 @@ describe('Test GET /launches', () => {
 });
 
 describe('Test POST /launches', () => {
-  test('It should respond with response 201 created', async () => {
-    const response = await request(app).post('/launches').send({
-      mission: 'Mission-101',
-      rocket: 'GSLV-16D',
-      target: 'Kepler-186 f',
-      launchDate: 'January 4, 2028',
-    }).expect('Content-Type', /json/).expect(201);
+  const completeLaunchData = {
+    mission: 'Mission-101',
+    rocket: 'GSLV-16D',
+    target: 'Kepler-186 f',
+    launchDate: 'January 4, 2028',
+  };
 
-    expect(response.body).toMatchObject({
-      mission: 'Mission-101',
-      rocket: 'GSLV-16D',
-      target: 'Kepler-186 f',
-      launchDate: 'January 4, 2028',
-    });
+  const LaunchDataWithoutDate = {
+    mission: 'Mission-101',
+    rocket: 'GSLV-16D',
+    target: 'Kepler-186 f',    
+  };
+
+  test('It should respond with response 201 created', async () => {
+    const response = await request(app).post('/launches').send(completeLaunchData).expect('Content-Type', /json/).expect(201);
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+
+    expect(response.body).toMatchObject(LaunchDataWithoutDate);
 
   });
 
