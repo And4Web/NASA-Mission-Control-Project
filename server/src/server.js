670ece2590bbc8332924d67613c2363f0d8201e5
Anand Shukla <anands2807@gmail.com>
2021-08-09 20:16:25 +0530

    mongoDB and mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,20 @@
 const http = require('http');
 const app = require('./app');
 const {loadPlanetsData} = require('./models/planets.model');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT ||8000;
+
+const MONGO_URL = 'mongodb+srv://nasa-api:e2dKPwdkUl5gHYHt@nasacluster.it8mq.mongodb.net/nasa?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
  
 async function startServer(){
 
+  await mongoose.connect(MONGO_URL, {
+    
+  });
   await loadPlanetsData();
   
   server.listen(PORT, () => {
