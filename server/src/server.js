156b58ebe8acb45191f5f058fa4f812f50bfc282
Anand Shukla <anands2807@gmail.com>
2021-08-20 15:36:54 +0530

    jest with mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -2,30 +2,17 @@
 const http = require('http');
 const app = require('./app');
 const {loadPlanetsData} = require('./models/planets.model');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
+
+const {mongoConnect} = require('./services/mongo');
 
 const PORT = process.env.PORT ||8000;
 
-const MONGO_URL = 'mongodb+srv://nasa-api:e2dKPwdkUl5gHYHt@nasacluster.it8mq.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
-
-mongoose.connection.once('open', ()=> {
-  console.log('MongoDB connection Ready.');
-});
-
-mongoose.connection.on('Error', (err)=> {
-  console.error(err);
-});
 
 async function startServer(){
 
-  await mongoose.connect(MONGO_URL, {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+  await mongoConnect();
   
   await loadPlanetsData();
   
